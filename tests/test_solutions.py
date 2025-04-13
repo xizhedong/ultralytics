caeea1d262db12addb1a109439c46eec7625b315
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-04-13 08:57:44 +0500

    Only import and define variables
    Signed-off-by: Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -23,34 +23,34 @@ REGION = [(10, 200), (540, 200), (540, 180), (10, 180)]  # for object counting, 
 
 # Test configs for each solution : (name, class, needs_frame_count, video, kwargs)
 SOLUTIONS = [
-    (
-        "ObjectCounter",
-        solutions.ObjectCounter,
-        False,
-        DEMO_VIDEO,
-        {"region": REGION, "model": MODEL, "show": SHOW},
-    ),
-    (
-        "Heatmap",
-        solutions.Heatmap,
-        False,
-        DEMO_VIDEO,
-        {"colormap": cv2.COLORMAP_PARULA, "model": MODEL, "show": SHOW, "region": None},
-    ),
-    (
-        "HeatmapWithRegion",
-        solutions.Heatmap,
-        False,
-        DEMO_VIDEO,
-        {"colormap": cv2.COLORMAP_PARULA, "region": REGION, "model": MODEL, "show": SHOW},
-    ),
-    (
-        "SpeedEstimator",
-        solutions.SpeedEstimator,
-        False,
-        DEMO_VIDEO,
-        {"region": REGION, "model": MODEL, "show": SHOW},
-    ),
+    # (
+    #     "ObjectCounter",
+    #     solutions.ObjectCounter,
+    #     False,
+    #     DEMO_VIDEO,
+    #     {"region": REGION, "model": MODEL, "show": SHOW},
+    # ),
+    # (
+    #     "Heatmap",
+    #     solutions.Heatmap,
+    #     False,
+    #     DEMO_VIDEO,
+    #     {"colormap": cv2.COLORMAP_PARULA, "model": MODEL, "show": SHOW, "region": None},
+    # ),
+    # (
+    #     "HeatmapWithRegion",
+    #     solutions.Heatmap,
+    #     False,
+    #     DEMO_VIDEO,
+    #     {"colormap": cv2.COLORMAP_PARULA, "region": REGION, "model": MODEL, "show": SHOW},
+    # ),
+    # (
+    #     "SpeedEstimator",
+    #     solutions.SpeedEstimator,
+    #     False,
+    #     DEMO_VIDEO,
+    #     {"region": REGION, "model": MODEL, "show": SHOW},
+    # ),
     # (
     #     "QueueManager",
     #     solutions.QueueManager,
@@ -134,37 +134,37 @@ SOLUTIONS = [
 ]
 
 
-def process_video(solution, video_path, needs_frame_count=False):
-    """Process video with solution, feeding frames and optional frame count."""
-    cap = cv2.VideoCapture(video_path)
-    assert cap.isOpened(), f"Error reading video file {video_path}"
+# def process_video(solution, video_path, needs_frame_count=False):
+#     """Process video with solution, feeding frames and optional frame count."""
+#     cap = cv2.VideoCapture(video_path)
+#     assert cap.isOpened(), f"Error reading video file {video_path}"
 
-    frame_count = 0
-    while cap.isOpened():
-        success, im0 = cap.read()
-        if not success:
-            break
-        frame_count += 1
-        im_copy = im0.copy()
-        args = [im_copy, frame_count] if needs_frame_count else [im_copy]
-        _ = solution(*args)
+#     frame_count = 0
+#     while cap.isOpened():
+#         success, im0 = cap.read()
+#         if not success:
+#             break
+#         frame_count += 1
+#         im_copy = im0.copy()
+#         args = [im_copy, frame_count] if needs_frame_count else [im_copy]
+#         _ = solution(*args)
 
-    cap.release()
+#     cap.release()
 
 
-# @pytest.mark.skipif(True, reason="Disabled for testing due to --slow test errors after YOLOE PR.")
-@pytest.mark.parametrize("name, solution_class, needs_frame_count, video, kwargs", SOLUTIONS)
-def test_solution(name, solution_class, needs_frame_count, video, kwargs):
-    """Test individual Ultralytics solution."""
-    if video:
-        safe_download(url=f"{ASSETS_URL}/{video}", dir=TMP)
-    if name == "ParkingManager":
-        safe_download(url=f"{ASSETS_URL}/{PARKING_AREAS_JSON}", dir=TMP)
-        safe_download(url=f"{ASSETS_URL}/{PARKING_MODEL}", dir=TMP)
-    solution = solution_class(**kwargs)
+# # @pytest.mark.skipif(True, reason="Disabled for testing due to --slow test errors after YOLOE PR.")
+# @pytest.mark.parametrize("name, solution_class, needs_frame_count, video, kwargs", SOLUTIONS)
+# def test_solution(name, solution_class, needs_frame_count, video, kwargs):
+#     """Test individual Ultralytics solution."""
+#     if video:
+#         safe_download(url=f"{ASSETS_URL}/{video}", dir=TMP)
+#     if name == "ParkingManager":
+#         safe_download(url=f"{ASSETS_URL}/{PARKING_AREAS_JSON}", dir=TMP)
+#         safe_download(url=f"{ASSETS_URL}/{PARKING_MODEL}", dir=TMP)
+#     solution = solution_class(**kwargs)
 
-    if name == "StreamlitInference":
-        if checks.check_imshow():  # requires interactive GUI environment
-            solution.inference()
-    else:
-        process_video(solution, str(TMP / video), needs_frame_count)
+#     if name == "StreamlitInference":
+#         if checks.check_imshow():  # requires interactive GUI environment
+#             solution.inference()
+#     else:
+#         process_video(solution, str(TMP / video), needs_frame_count)
